20909e29a107629af197c4ff57c9a51535a2377c
Florent Garcia <florent.garcia@oclock.school>
2024-09-03 23:58:49 +0200

    feat: a first html of header

--- src/components/App/Header/Header.tsx
+++ src/components/App/Header/Header.tsx
@@ -1,3 +1,35 @@
-function Header() {}
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <div className="header">
+            {/* logo */}
+            <div className="logo-container">
+                <Link to="/">Ciné Délices</Link>
+            </div>
+            <div className="link-container">
+                {/* barre de raccourci */}
+                <div className="account-icon">Bonhomme</div>
+                <div className="add-recipe-icon">+</div>
+                <div className="search-recipe-icon">Loupe</div>
+                {/* burger menu */}
+                <div className="burger-menu">
+                    <div className="line"></div>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                </div>
+            </div>
+            {/* <ul>
+                <li>Accueil</li>
+                <li>Catalogue</li>
+                <li>Connexion</li>
+                <li>Inscription</li>
+                <li>Proposer une recette</li>
+                <li>Recherche</li>
+            </ul> */}
+        </div>
+    );
+}
 
 export default Header;
