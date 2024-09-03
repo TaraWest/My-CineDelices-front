c5264ba9ac0e855e1876a932968571e32d4b6a30
quentin-garcia017 <quentin.garcia17@orange.fr>
2024-09-03 09:05:33 +0200

    feat: add tailwind css (grid card)

--- src/components/App/Routes/CatalogPage/pages/index.tsx
+++ src/components/App/Routes/CatalogPage/pages/index.tsx
@@ -1,18 +1,28 @@
 import { useState } from 'react';
 import { Recipes } from '../models';
 import { getRecipes } from '../services';
+import './index.scss';
 
 export const Catalog = () => {
     const [recipes, setRecipes] = useState<Recipes>(getRecipes);
 
     return (
         <>
-            <h1>Catalogue</h1>
-            <div>
+            <h1 className="text-2xl font-bold mb-4">Catalogue</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4">
                 {recipes.map((recipe) => (
-                    <div key={recipe.id}>
-                        <h2>{recipe.name}</h2>
-                        <img src={recipe.picture} alt={recipe.name} />
+                    <div
+                        key={recipe.id}
+                        className="bg-white shadow rounded-lg p-4"
+                    >
+                        <h2 className="text-lg font-semibold mb-2">
+                            {recipe.name}
+                        </h2>
+                        <img
+                            src={recipe.picture}
+                            alt={recipe.name}
+                            className="h-32 w-full object-cover rounded-md"
+                        />
                     </div>
                 ))}
             </div>
