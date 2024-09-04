31458a86e492983c5b64e8548a53745801157dce
Stephsas <stephanie0marchand@gmail.com>
2024-09-04 16:12:33 +0200

    Adding a Burger Menu with React and Tailwind

--- src/components/App/Header/Header.tsx
+++ src/components/App/Header/Header.tsx
@@ -1,46 +1,67 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-    faUser,
-    faPlus,
-    faSearch,
-    faBars,
-} from '@fortawesome/free-solid-svg-icons';
+import { faUser, faPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
     return (
-        <div className="header">
+        <div className="header flex justify-between items-center p-4 bg-custom-dark-red text-white">
             {/* logo */}
             <div className="logo-container">
-                <Link to="/">Ciné Délices</Link>
+                <Link to="/" className="text-2l font-bold">
+                    Ciné Délices
+                </Link>
             </div>
-            <div className="link-container">
+            <div className="link-container flex items-center space-x-4">
                 {/* barre de raccourci */}
-                <div className="account-icon">
+                <div className="icon">
                     <FontAwesomeIcon icon={faUser} />
                 </div>
-                <div className="add-recipe-icon">
+                <div className="icon">
                     <FontAwesomeIcon icon={faPlus} />
                 </div>
-                <div className="search-recipe-icon">
+                <div className="icon">
                     <FontAwesomeIcon icon={faSearch} />
                 </div>
                 {/* burger menu */}
-                <div className="burger-menu">
-                    <div className="line"></div>
-                    <div className="line"></div>
-                    <div className="line"></div>
-                    <div className="line"></div>
+                <div
+                    className="burger-menu flex flex-col cursor-pointer"
+                    onClick={toggleMenu}
+                >
+                    <div className="line bg-custom-beige"></div>
+                    <div className="line bg-custom-beige"></div>
+                    <div className="line bg-custom-beige"></div>
                 </div>
             </div>
-            {/* <ul>
-                <li>Accueil</li>
-                <li>Catalogue</li>
-                <li>Connexion</li>
-                <li>Inscription</li>
-                <li>Proposer une recette</li>
-                <li>Recherche</li>
-            </ul> */}
+            {/* menu mobile */}
+            <div
+                className={`mobile-menu absolute top-0 right-0 mt-16 bg-custom-dark-red text-white w-64 transition-transform duration-300 ${isMenuOpen ? 'transform translate-x-0' : 'transform translate-x-full'}`}
+            >
+                <div className="p-4">
+                    <Link to="/" className="block py-2">
+                        Accueil
+                    </Link>
+                    <Link to="/catalog" className="block py-2">
+                        Catalogue
+                    </Link>
+                    <Link to="/connexion" className="block py-2">
+                        Connexion
+                    </Link>
+                    <Link to="/inscription" className="block py-2">
+                        Inscription
+                    </Link>
+                    <Link to="/addrecipe" className="block py-2">
+                        Proposer une recette
+                    </Link>
+                    <Link to="/recherche" className="block py-2">
+                        Recherche
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                'custom-dark-red': '#59041b',
+                'custom-beige': '#D9C7B8',
+            },
+        },
+    },
+    plugins: [],
+};
