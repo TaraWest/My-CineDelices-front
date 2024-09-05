dd1a936db0d3d2c130e18e74e0214e75a8d7563f
Florent Garcia <florent.garcia@oclock.school>
2024-09-05 13:37:33 +0200

    feat: commit for meeting

--- src/components/App/Routes/RecipePage/RecipePage.tsx
+++ src/components/App/Routes/RecipePage/RecipePage.tsx
@@ -42,12 +42,14 @@ function RecipePage() {
                 <div className="images-container">
                     <img
                         className="recipe-page-image image-dish"
-                        src={dataFetch.picture}
+                        // src={dataFetch.picture}
+                        src="/recipes/ramen.png"
                         alt="Photo illustrant la recette"
                     />
                     <img
                         className="recipe-page-image image-film"
-                        src={dataFetch.Movie.picture}
+                        // src={dataFetch.Movie.picture}
+                        src="/movies/naruto.png"
                         alt="Photo illustrant le film"
                     />
                 </div>
