e58d93cdda1a340e6211578b5b619addddc4150c
Stephsas <stephanie0marchand@gmail.com>
2024-09-05 13:08:54 +0200

    adding research bar mobile

--- src/components/App/Header/Header.tsx
+++ src/components/App/Header/Header.tsx
@@ -2,46 +2,67 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
+import './header.scss';
 
 function Header() {
-    // est ce que mon burger menu est ouvert ou fermé
+    // État pour savoir si le menu burger est ouvert ou fermé
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    // fonction pour basculer l'état du menu ouvert ou fermé
+    // État pour savoir si la barre de recherche est ouverte ou fermée
+    const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+    // Fonction pour basculer l'état du menu burger
     const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
+    // Fonction pour basculer l'état de la barre de recherche
+    const toggleSearch = () => setIsSearchOpen(!isSearchOpen);
+
     return (
-        <div className="header flex justify-between items-center p-4 bg-custom-dark-red text-white border-b-2 border-custom-beige">
+        <div className="header flex justify-between items-center p-4 bg-custom-dark-red text-white border-b-2 relative">
             {/* logo */}
             <div className="logo-container">
-                <Link to="/" className="text-2l font-bold">
+                <Link to="/" className="text-2xl font-bold ">
                     Ciné Délices
                 </Link>
             </div>
-            <div className="link-container flex items-center space-x-4">
-                {/* barre de raccourci */}
+            <div className="link-container flex items-center space-x-4 relative">
+                {/* Icônes de raccourci */}
                 <div className="icon">
                     <FontAwesomeIcon icon={faUser} />
                 </div>
                 <div className="icon">
                     <FontAwesomeIcon icon={faPlus} />
                 </div>
-                <div className="icon">
+                {/* Icône de recherche avec gestionnaire de clic */}
+                <div
+                    className="icon md:hidden cursor-pointer"
+                    onClick={toggleSearch}
+                >
                     <FontAwesomeIcon icon={faSearch} />
                 </div>
-                {/* burger menu */}
+                {/* Menu burger pour mobile */}
                 <div
-                    className="burger-menu flex flex-col cursor-pointer"
+                    className="burger-menu flex flex-col cursor-pointer md:hidden"
                     onClick={toggleMenu}
                 >
                     <div className="line bg-custom-beige"></div>
                     <div className="line bg-custom-beige"></div>
                     <div className="line bg-custom-beige"></div>
                 </div>
+                {/* Barre de recherche */}
+                <div
+                    className={`search-bar absolute top-16 right-0 bg-white text-black w-64 transition-transform duration-300 ${isSearchOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 md:relative md:w-auto md:bg-transparent md:text-white`}
+                >
+                    <input
+                        type="text"
+                        placeholder="Rechercher..."
+                        className="w-full p-2"
+                    />
+                </div>
             </div>
             {/* Menu mobile qui s'affiche ou se cache selon l'état isMenuOpen */}
             <div
-                className={`mobile-menu absolute top-0 right-0 mt-16 bg-custom-dark-red text-white w-64 transition-transform duration-300 ${isMenuOpen ? 'transform translate-x-0' : 'transform translate-x-full'}`}
+                className={`mobile-menu absolute top-0 right-0 mt-16 bg-custom-dark-red w-64 transition-transform duration-300 ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}`}
             >
                 <div className="p-4">
                     <Link to="/" className="block py-2">
