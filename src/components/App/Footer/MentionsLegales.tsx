0f189c784712268ae06be73971125f6d6ffe0e54
Stephsas <stephanie0marchand@gmail.com>
2024-09-17 15:04:30 +0200

    updated legalnotice page

--- src/components/App/Footer/MentionsLegales.tsx
+++ src/components/App/Footer/MentionsLegales.tsx
@@ -1,30 +1,62 @@
 export const LegalNotice = () => {
     return (
-        <div>
-            {' '}
-            © 2024 [Nom de Votre Projet]. Tous droits réservés. Le site et son
-            contenu, y compris tous les éléments graphiques, sont protégés par
-            les lois sur la propriété intellectuelle. Toute reproduction,
-            distribution, modification ou utilisation de tout ou partie du site
-            sans autorisation préalable est strictement interdite. Pour toute
-            demande, veuillez nous contacter via la page dédiée. 1. Informations
-            sur l’Entreprise [Nom de l’Entreprise] [Adresse] [Ville, Code
-            Postal] [Pays] [Adresse Email] [Numéro de Téléphone] 2. Hébergement
-            [Nom de l’Hébergeur] [Adresse de l’Hébergeur] [Ville, Code Postal]
-            [Pays] [Adresse Email] [Numéro de Téléphone] 3. Conditions
-            d’Utilisation En accédant et en utilisant ce site, vous acceptez de
-            respecter ces termes et conditions. Le contenu et les services
-            fournis sont uniquement à titre d’information et ne peuvent être
-            utilisés à des fins commerciales sans autorisation expresse. 4.
-            Politique de Confidentialité Nous respectons votre vie privée et
-            nous nous engageons à protéger vos données personnelles. Veuillez
-            consulter notre Politique de Confidentialité pour plus de détails
-            sur la manière dont nous collectons, utilisons et protégeons vos
-            informations. 5. Droits de Propriété Intellectuelle Tous les
-            contenus, marques et autres droits de propriété intellectuelle
-            affichés sur ce site sont la propriété de [Nom de l’Entreprise] ou
-            de leurs propriétaires respectifs. Toute utilisation non autorisée
-            de ces éléments est interdite.
-        </div>
+        <section className="p-6 max-w-4xl mx-auto">
+            <h1 className="text-3xl font-bold mb-6">Mentions Légales</h1>
+            <p className="mb-4">
+                © 2024 [Nom de Votre Projet]. Tous droits réservés. Le site et
+                son contenu, y compris tous les éléments graphiques, sont
+                protégés par les lois sur la propriété intellectuelle. Toute
+                reproduction, distribution, modification ou utilisation de tout
+                ou partie du site sans autorisation préalable est strictement
+                interdite. Pour toute demande, veuillez nous contacter via la
+                page dédiée.
+            </p>
+            <h2 className="text-2xl font-semibold mb-4">
+                Informations sur l’Entreprise
+            </h2>
+            <ul className="list-disc list-inside mb-6">
+                <li>CinéDélices</li>
+                <li>8 rue des gourmets</li>
+                <li>75016 PancakesCity</li>
+                <li>France</li>
+                <li>cinedelices@gmail.com</li>
+                <li>0156824500</li>
+            </ul>
+            <h2 className="text-2xl font-semibold mb-4">Hébergement</h2>
+            <ul className="list-disc list-inside mb-6">
+                <li>La Cuisine Numérique S.A</li>
+                <li>123 rue du cinéma</li>
+                <li>Gourdmandville</li>
+                <li>contact@cuisinenumerique</li>
+                <li>0198101045</li>
+            </ul>
+            <h2 className="text-2xl font-semibold mb-4">
+                Conditions d’Utilisation
+            </h2>
+            <p className="mb-4">
+                En accédant et en utilisant ce site, vous acceptez de respecter
+                ces termes et conditions. Le contenu et les services fournis
+                sont uniquement à titre d’information et ne peuvent être
+                utilisés à des fins commerciales sans autorisation expresse.
+            </p>
+            <h2 className="text-2xl font-semibold mb-4">
+                Politique de Confidentialité
+            </h2>
+            <p className="mb-4">
+                Nous respectons votre vie privée et nous nous engageons à
+                protéger vos données personnelles. Veuillez consulter notre
+                Politique de Confidentialité pour plus de détails sur la manière
+                dont nous collectons, utilisons et protégeons vos informations.
+            </p>
+            <h2 className="text-2xl font-semibold mb-4">
+                Droits de Propriété Intellectuelle
+            </h2>
+            <p>
+                Tous les contenus, marques et autres droits de propriété
+                intellectuelle affichés sur ce site sont la propriété de [Nom de
+                l’Entreprise] ou de leurs propriétaires respectifs. Toute
+                utilisation non autorisée de ces éléments est interdite.
+            </p>
+        </section>
     );
 };
