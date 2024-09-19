0e28e53cb82d9779f0ed6012dcca1fc3c74ec843
Stephsas <stephanie0marchand@gmail.com>
2024-09-19 20:54:25 +0200

    add CSS into 404NotFound

--- src/components/App/Routes/404NotFound/PageNotFound.tsx
+++ src/components/App/Routes/404NotFound/PageNotFound.tsx
@@ -4,18 +4,18 @@ import './PageNotFound.scss';
 function PageNotFound() {
     return (
         <div className="flex flex-col items-center">
-            <p className="mt-2em font-cinzel text-2xl font-semibold">
+            <p className="mt-1em mx-2em font-cinzel text-1xl font-semibold">
                 Oh non ! la page que vous cherchez est aussi introuvable qu'un
                 ramen dans un film de science-fiction...
             </p>
             <div className="not-found-part">
                 <img
                     className="not-found-img"
-                    src="http://localhost:3000/notFound.webp"
+                    src="http://localhost:3000/recipes/ramen.webp"
                     alt=""
                 />
             </div>
-            <Link className="text-2xl" to="/catalogue">
+            <Link className="text-2xl mx-2em" to="/catalogue">
                 Cliquez ici pour revenir à la page de catalogue!
             </Link>
         </div>
--- src/components/App/Routes/HomePage/HomePage.tsx
+++ src/components/App/Routes/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { IRecipe } from './models';
 import Slider from './components/slider/slider';
@@ -6,11 +6,13 @@ import { fetchOneRandom } from './services';
 import './components/slider/slider.scss';
 import './homePage.scss';
 import AddRecipeModal from '../CatalogPage/components/modal';
+import { useAuthContext } from '../../Context/Authentification/useAuthContext';
+import { useMediaQuery } from 'react-responsive';
 
 function HomePage() {
     const [data, setData] = useState<IRecipe | null>(null);
-
-    const navigate = useNavigate();
+    const isDesktop = useMediaQuery({ query: '(min-width: 768px)' });
+    const { userAuth } = useAuthContext();
 
     // Fonction pour gérer l'ajout d'une recette
     const handleAddRecipe = (newRecipe: any) => {
@@ -34,70 +36,58 @@ function HomePage() {
 
     return (
         <div className="homePage">
+            {userAuth?.username && !isDesktop && (
+                <div className={` text-right text-skin text-base w-full mr-10`}>
+                    Bienvenue {userAuth.username} !
+                </div>
+            )}
             <h1 className="homepage-title">
                 Bienvenue dans Ciné Délices !
                 <br />
                 Plongez dans un univers où la cuisine rencontre le cinéma...
             </h1>
-            <h2 className="homepage-subtitle mb-2em">
+            <h2 className="homepage-subtitle">
                 Pas d'idée pour ce soir ? Trouve l'inspiration avec des recettes
                 tirées de tes films et séries préférés !
             </h2>
 
-            <div className="w-11/12 ">
-                <div className="container">
-                    <div className="presentation">
-                        {/* for middle devices */}
-                        <img
-                            className="homepage-image-desktop"
-                            src="http://localhost:3000/movies/Cinédelices.webp"
-                            alt=""
-                        />
-                        <p>
-                            Bienvenue sur CinéDélices, votre destination
-                            incontournable pour découvrir des recettes inspirées
-                            des films cultes ! Plongez dans l'univers
-                            cinématographique à travers les plats emblématiques
-                            qui ont marqué l'histoire du grand écran. Chaque
-                            recette est soigneusement recréée pour vous
-                            permettre de goûter à la magie des scènes les plus
-                            gourmandes. Que vous soyez fan de banquets
-                            fantastiques, de festins royaux ou de petits plats
-                            simples mais mémorables, nous vous invitons à
-                            cuisiner comme vos personnages préférés.
-                            Préparez-vous à allier passion du cinéma et plaisir
-                            de la table ! Bon appétit et bon film !
-                        </p>
-                    </div>
-                    <div className="img_presentation">
-                        <img
-                            className="homepage-image"
-                            src="http://localhost:3000/movies/Cinédelices.webp"
-                            alt=""
-                        />
-                    </div>
+            <ul className="presentation-list">
+                <li className="presentation-list-item">
+                    <Link to="/catalogue" className="button-link">
+                        Nos recettes
+                    </Link>
+                </li>
+                <li className="presentation-list-item">
+                    <AddRecipeModal onAddRecipe={handleAddRecipe} />{' '}
+                </li>
+            </ul>
+
+            <div className="container">
+                <div className="presentation">
+                    <p>
+                        Bienvenue sur CinéDélices, votre destination
+                        incontournable pour découvrir des recettes inspirées des
+                        films cultes ! Plongez dans l'univers cinématographique
+                        à travers les plats emblématiques qui ont marqué
+                        l'histoire du grand écran. Chaque recette est
+                        soigneusement recréée pour vous permettre de goûter à la
+                        magie des scènes les plus gourmandes. Que vous soyez fan
+                        de banquets fantastiques, de festins royaux ou de petits
+                        plats simples mais mémorables, nous vous invitons à
+                        cuisiner comme vos personnages préférés. Préparez-vous à
+                        allier passion du cinéma et plaisir de la table ! Bon
+                        appétit et bon film !
+                    </p>
+                </div>
+
+                <div className="img_presentation">
+                    <img
+                        className="homepage-image"
+                        src="http://localhost:3000/movies/Cinédelices.webp"
+                        alt=""
+                    />
                 </div>
             </div>
-            <div className="homepage-button">
-                <button
-                    className="button-link homepage-link-button"
-                    onClick={() => {
-                        navigate('/catalogue');
-                    }}
-                >
-                    Nos recettes
-                </button>
-                <AddRecipeModal onAddRecipe={handleAddRecipe} />{' '}
-            </div>
-
-            {/* <ul >
-                <li className="presentation-list-item">
-                    
-                </li>
-                <li className="presentation-list-item">
-                   
-                </li>
-            </ul> */}
 
             <h4>
                 Met ton plus beau tablier et prépare ta soirée avec notre
