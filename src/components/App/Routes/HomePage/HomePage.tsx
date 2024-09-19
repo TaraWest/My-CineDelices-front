6c80663c34d653c225782063e60e776a13857a30
Florent Garcia <florent.garcia@oclock.school>
2024-09-19 15:35:19 +0200

    feat: searchbar for header is ok, homepage is responsive, button under presentation image, standardized, border around presentation and disposition change dependent on the media device in the presentation

--- src/components/App/Routes/HomePage/HomePage.tsx
+++ src/components/App/Routes/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { IRecipe } from './models';
 import Slider from './components/slider/slider';
@@ -6,13 +6,11 @@ import { fetchOneRandom } from './services';
 import './components/slider/slider.scss';
 import './homePage.scss';
 import AddRecipeModal from '../CatalogPage/components/modal';
-import { useAuthContext } from '../../Context/Authentification/useAuthContext';
-import { useMediaQuery } from 'react-responsive';
 
 function HomePage() {
     const [data, setData] = useState<IRecipe | null>(null);
-    const isDesktop = useMediaQuery({ query: '(min-width: 768px)' });
-    const { userAuth } = useAuthContext();
+
+    const navigate = useNavigate();
 
     // Fonction pour gérer l'ajout d'une recette
     const handleAddRecipe = (newRecipe: any) => {
@@ -41,47 +39,64 @@ function HomePage() {
                 <br />
                 Plongez dans un univers où la cuisine rencontre le cinéma...
             </h1>
-            <h2 className="homepage-subtitle">
+            <h2 className="homepage-subtitle mb-2em">
                 Pas d'idée pour ce soir ? Trouve l'inspiration avec des recettes
                 tirées de tes films et séries préférés !
             </h2>
-            <ul className="presentation-list">
-                <li className="presentation-list-item">
-                    <Link to="/catalogue" className="button-link">
-                        Nos recettes
-                    </Link>
-                </li>
-                <li className="presentation-list-item">
-                    <AddRecipeModal onAddRecipe={handleAddRecipe} />{' '}
-                </li>
-            </ul>
 
-            <div className="container">
-                <div className="presentation">
-                    <p>
-                        Bienvenue sur CinéDélices, votre destination
-                        incontournable pour découvrir des recettes inspirées des
-                        films cultes ! Plongez dans l'univers cinématographique
-                        à travers les plats emblématiques qui ont marqué
-                        l'histoire du grand écran. Chaque recette est
-                        soigneusement recréée pour vous permettre de goûter à la
-                        magie des scènes les plus gourmandes. Que vous soyez fan
-                        de banquets fantastiques, de festins royaux ou de petits
-                        plats simples mais mémorables, nous vous invitons à
-                        cuisiner comme vos personnages préférés. Préparez-vous à
-                        allier passion du cinéma et plaisir de la table ! Bon
-                        appétit et bon film !
-                    </p>
-                </div>
-
-                <div className="img_presentation">
-                    <img
-                        className="homepage-image"
-                        src="http://localhost:3000/movies/Cinédelices.webp"
-                        alt=""
-                    />
+            <div className="w-11/12 ">
+                <div className="container">
+                    <div className="presentation">
+                        <img
+                            className="homepage-image-desktop"
+                            src="http://localhost:3000/movies/Cinédelices.webp"
+                            alt=""
+                        />
+                        <p>
+                            Bienvenue sur CinéDélices, votre destination
+                            incontournable pour découvrir des recettes inspirées
+                            des films cultes ! Plongez dans l'univers
+                            cinématographique à travers les plats emblématiques
+                            qui ont marqué l'histoire du grand écran. Chaque
+                            recette est soigneusement recréée pour vous
+                            permettre de goûter à la magie des scènes les plus
+                            gourmandes. Que vous soyez fan de banquets
+                            fantastiques, de festins royaux ou de petits plats
+                            simples mais mémorables, nous vous invitons à
+                            cuisiner comme vos personnages préférés.
+                            Préparez-vous à allier passion du cinéma et plaisir
+                            de la table ! Bon appétit et bon film !
+                        </p>
+                    </div>
+                    <div className="img_presentation">
+                        <img
+                            className="homepage-image"
+                            src="http://localhost:3000/movies/Cinédelices.webp"
+                            alt=""
+                        />
+                    </div>
                 </div>
             </div>
+            <div className="homepage-button">
+                <button
+                    className="button-link homepage-link-button"
+                    onClick={() => {
+                        navigate('/catalogue');
+                    }}
+                >
+                    Nos recettes
+                </button>
+                <AddRecipeModal onAddRecipe={handleAddRecipe} />{' '}
+            </div>
+
+            {/* <ul >
+                <li className="presentation-list-item">
+                    
+                </li>
+                <li className="presentation-list-item">
+                   
+                </li>
+            </ul> */}
 
             <h4>
                 Met ton plus beau tablier et prépare ta soirée avec notre
